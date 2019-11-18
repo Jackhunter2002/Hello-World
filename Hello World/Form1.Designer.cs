460e80b68c56f1a6538400c583927b946310c634
Jack Hunter <rc0037207@student.riversidecollege.ac.uk>
2019-11-18 15:08:21 +0000

    I added a text box as well as a message saying hello world

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtbxName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(238, 67);
+            this.button1.Location = new System.Drawing.Point(238, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(259, 125);
             this.button1.TabIndex = 0;
@@ -41,23 +42,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // txtbxName
+            // 
+            this.txtbxName.Location = new System.Drawing.Point(238, 143);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(259, 20);
+            this.txtbxName.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GhostWhite;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbxName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtbxName;
     }
 }
 
